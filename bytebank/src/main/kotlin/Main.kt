7831cf5ac479a7f6d6391a10ebb9d631548b4d09
Northon <northon.utfpr@gmail.com>
2022-04-25 15:05:08 -0300

    feat: A new class: Account, with two methods: Withdraw and Deposit

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -1,5 +1,34 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-    val teste: String = "Teste";
-    println(teste)
+    val account = Account(
+        "Northon",
+        100
+    )
+    account.deposit(1000.0)
+    account.withdraw(801.0)
 }
+
+
+data class Account(val owner: String, val code: Int, var ballance: Double? = 0.0, var multipleLimit: Double = 0.8) {
+    fun deposit(ammount: Double = 0.0, depositor: Int? = 0) {
+        ballance = ballance?.plus(ammount)
+    }
+
+    fun withdraw(ammount: Double = 0.0, destination: String? = "Never Where") {
+        if (ammount < ballance!!.times(multipleLimit)) {
+            ballance = ballance?.minus(ammount)
+            println("SUCCESS")
+            println("WITHDRAW $ammount TO $destination")
+            println("YOUR BALLANCE IS ACTUALLY $ballance")
+        } else {
+            println("HIGH VALUE WITHDRAW!!!")
+            println("CONTACT THE MANAGER OF YOUR ACCOUNT TO REALIZE THAT WITHDRAW")
+            println("MANAGER EMAIL: northon.contact@gmail.com")
+        }
+    }
+
+
+    fun transfer(ammount: Double = 0.0, receptor: Int? = 0) {
+
+    }
+}
+
